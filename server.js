91ddc1069da46884f4478e0cddfee1ee14909709
Matheus Alencar <matheusalencar6942@gmail.com>
2025-07-10 20:32:18 -0300

    gerando página de agradecimentos de forma dinâmica

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const PORT = 3000;
@@ -10,6 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use("/views", express.static(path.join(__dirname, "views")));
 
+app.get("/contato", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "contato.html"));
+});
+
 app.get("/sugestao", (req, res) => {
   const nome = req.query.nome?.trim();
   const ingredientes = req.query.ingredientes?.trim();
@@ -17,10 +22,17 @@ app.get("/sugestao", (req, res) => {
   if (!ingredientes) console.log("O campo [ingredientes] é obrigatório");
 
   if (nome && ingredientes) {
-    console.log(`Nome: ${nome}\nIngredientes: ${ingredientes}`);
+    gerarPaginaAgradecimento(
+      "Agradeçemos sua sugestão",
+      `
+        <ul>
+          <li>Nome: ${nome}</li>
+          <li>Ingredientes: ${ingredientes}</li>
+        <ul>
+      `,
+      res
+    );
   }
-
-  res.redirect("views/agradecimento.html");
 });
 
 app.post("/contato", (req, res) => {
@@ -32,12 +44,23 @@ app.post("/contato", (req, res) => {
   if (!mensagem) console.log("O campo [mensagem] é obrigatório");
 
   if (nome && email && assunto && mensagem) {
-    console.log(
-      `Nome: ${nome}\nEmail: ${email}\nAssunto: ${assunto}\nMensagem: ${mensagem}`
+    gerarPaginaAgradecimento(
+      "Agradeçemos o contato, retornaresmo o mais rápido possível",
+      `
+        <ul>
+          <li>Nome: ${nome}</li>
+          <li>Email: ${email}</li>
+          <li>Assunto: ${assunto}</li>
+          <li>Mensagem: ${mensagem}</li>
+        <ul>
+      `,
+      res
     );
   }
+});
 
-  res.redirect("views/agradecimento.html");
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 app.use((req, res, next) => {
@@ -47,3 +70,25 @@ app.use((req, res, next) => {
 app.listen(PORT, () => {
   console.log(`DevBurger server is running on port ${PORT}`);
 });
+
+function gerarPaginaAgradecimento(title, content, res) {
+  fs.readFile(
+    path.join(__dirname, "views", "agradecimento.html"),
+    "utf8",
+    (err, html) => {
+      if (err) {
+        return res.status(500).send("Erro ao carregar a página.");
+      }
+
+      let newHtml = html;
+
+      newHtml = html
+        .replace("{{title}}", title)
+        .replace("{{content}}", content);
+
+      // newHtml = html.replace("{{title}}", "").replace("{{content}}", "");
+
+      res.send(newHtml);
+    }
+  );
+}
